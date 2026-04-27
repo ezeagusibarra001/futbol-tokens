a27aab31c4e01ff1e4e08c0eaa59735a6fd91fdf
MAURO ROMAN AGUERO ACOSTA <85760409+magueroacosta4@users.noreply.github.com>
2026-04-27 16:04:58 -0300

    scrapper de liga y equipo

---
+++ src/modules/player/player.scrapper.ts
@@ -0,0 +1,248 @@
+import { Page } from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+puppeteer.use(StealthPlugin());
+
+
+const preActions = async () => {
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-blink-features=AutomationControlled"
+  ]
+});
+
+  const page = await browser.newPage();
+
+await page.setExtraHTTPHeaders({
+  "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+});
+
+  await page.goto("https://www.whoscored.com/", {
+    waitUntil: "networkidle2"
+  });
+
+  await page.waitForFunction(() => document.readyState === "complete");
+
+  await page.evaluate(() => {
+    const buttons = Array.from(document.querySelectorAll("button"));
+
+    const accept = buttons.find(b =>
+      b.textContent?.toLowerCase().includes("accept") ||
+      b.textContent?.toLowerCase().includes("aceptar")
+    );
+
+    if (accept) {
+      (accept as HTMLElement).click();
+    }
+  });
+
+  return { browser, page };
+};
+
+const findLink = async (page : Page, leagueName: string, querySelector: string) => {
+  return await page.evaluate((leagueName: string, querySelector: string) => {
+    const grid = document.querySelector(querySelector);
+    if (!grid) return null;
+
+    const links = grid.querySelectorAll("a");
+
+    for (const el of links) {
+      const text = el.textContent?.trim().toLowerCase();
+      if (text && text.includes(leagueName.toLowerCase())) {
+        return el.href;
+      } ;
+    }
+
+    return null;
+  }, leagueName, querySelector);
+};
+
+export const getPlayersFromTeamAndLeague = async (leagueName: string, team: string) => {
+  const { browser, page } = await preActions();
+// 🔥 abrir dropdown de torneos (sin usar ID)
+await page.waitForFunction(() => {
+  return Array.from(document.querySelectorAll("a, button"))
+    .some(el => el.textContent?.toLowerCase().includes("tournament"));
+});
+
+await page.evaluate(() => {
+  const btn = Array.from(document.querySelectorAll("a, button"))
+    .find(el => el.textContent?.toLowerCase().includes("tournament"));
+
+  if (btn) (btn as HTMLElement).click();
+});
+
+// 🔥 esperar que el dropdown tenga MUCHOS links visibles
+await page.waitForFunction(() => {
+  const visibleLinks = Array.from(document.querySelectorAll("a"))
+    .filter(a => (a as HTMLElement).offsetParent !== null);
+
+  return visibleLinks.length > 50; // dropdown cargado
+});
+
+
+const links = await page.$$("a");
+
+let tournamentLink: string | null = null;
+
+for (const link of links) {
+  const text = await link.evaluate(el => el.textContent?.toLowerCase() || "");
+
+  if (text.includes(leagueName.toLowerCase())) {
+    tournamentLink = await link.evaluate(el => (el as HTMLAnchorElement).href);
+    break;
+  }
+}
+if (tournamentLink) {
+  await page.goto(tournamentLink, { waitUntil: "networkidle2" });
+}
+
+  await page.waitForSelector("table");
+  const teamStatiscticLink = await findLink(page, "Team Statistics", "#sub-navigation");
+  if (teamStatiscticLink) {
+    await page.goto(teamStatiscticLink, { waitUntil: "networkidle2" });
+  }
+  
+  await page.waitForSelector("table");
+  const teamLink = await findLink(page, team, "table");
+  if (teamLink) {
+    await page.goto(teamLink, { waitUntil: "networkidle2" });
+  }
+
+
+await page.evaluate((leagueName: string, querySelector: string) => {
+    const grid = document.querySelector(querySelector);
+    if (!grid) return null;
+
+    const links = grid.querySelectorAll("a");
+
+    for (const el of links) {
+      const text = el.textContent?.trim().toLowerCase();
+      if (text && text.includes(leagueName.toLowerCase())) {
+        return el.href;
+      } ;
+    }
+
+    return null;
+  }, team, "table");
+
+
+const jugadores = await page.evaluate(() => {
+  const tables = Array.from(document.querySelectorAll("table"));
+  const tablaJugadores = tables.find(table => {
+    const header = table.innerText.toLowerCase();
+    return header.includes("player");
+  });
+
+  if (!tablaJugadores) return [];
+
+  const rows = tablaJugadores.querySelectorAll("tbody tr");
+
+  return Array.from(rows).map(row => {
+    const cols = row.querySelectorAll("td");
+
+    return {
+      nombre: cols[1]
+  ?.querySelector("a")
+  ?.textContent
+  ?.trim(),
+      goals: cols[6]?.innerText.trim(),
+      assists: cols[7]?.innerText.trim(),
+      shots : cols[10]?.innerText.trim(),
+      ratings: cols[14]?.innerText.trim(),
+       keyPasses: "-",
+      dribbles: "-",
+      tackles: "-",
+    };
+  });
+});
+const jugadoresMap = new Map(
+  jugadores.map(j => [j.nombre!!.toLowerCase(), j])
+);
+
+const cambiarTipoEstadistica = async (
+  tab: string,
+  columnaEsperada: string
+) => {
+  await page.evaluate((tab: string) => {
+    const container = document.querySelector("#team-squad-stats");
+    if (!container) return;
+
+    const tabs = Array.from(container.querySelectorAll("a, button"));
+
+    const btn = tabs.find(el =>
+      el.textContent?.toLowerCase().includes(tab.toLowerCase())
+    );
+
+    if (btn) (btn as HTMLElement).click();
+  }, tab);
+
+  await page.waitForFunction((columnaEsperada) => {
+    const container = document.querySelector("#team-squad-stats");
+    if (!container) return false;
+
+    const headers = Array.from(
+      container.querySelectorAll("table thead th")
+    ).map(th => th.textContent?.toLowerCase());
+
+    return headers.some(h => h?.includes(columnaEsperada.toLowerCase()));
+  }, {}, columnaEsperada);
+};
+
+const agregarPorClase = async (campo: string, className: string) => {
+  const data = await page.evaluate((className) => {
+    const container = document.querySelector("#team-squad-stats");
+    if (!container) return [];
+
+    const rows = container.querySelectorAll("table tbody tr");
+
+    return Array.from(rows).map(row => {
+      const nombre = row.querySelector("td:nth-child(2) a")
+        ?.textContent?.trim();
+
+      const valor = row.querySelector(`[class*="${className}"]`)
+        ?.textContent?.trim();
+
+      return {
+        nombre,
+        valor: Number(valor?.replace(/[^\d.]/g, "") || 0)
+      };
+    });
+  }, className);
+
+  for (const item of data) {
+    if (!item.nombre) continue;
+
+    const jugador = jugadoresMap.get(item.nombre.toLowerCase());
+
+    if (jugador) {
+      (jugador as any)[campo] = item.valor;
+    }
+  }
+};
+
+// Defensive
+await cambiarTipoEstadistica("defensive", "tackles");
+await agregarPorClase("tackles", "tacklePerGame");
+
+// Offensive
+await cambiarTipoEstadistica("offensive", "key");
+await agregarPorClase("keyPasses", "keyPassPerGame");
+
+await agregarPorClase("dribbles", "dribbleWonPerGame");
+
+await browser.close();
+return jugadores;
+
+}
+
+
+
+const getPlayersWithoutTeamParameterLink = async (leagueName: string) => {};
+
